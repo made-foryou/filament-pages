e4edf5b8683642c605358887e9a3d6e146181f57
Menno Tempelaar <menno@made-foryou.nl>
2024-03-02 13:25:54 +0100

    feat: setup base package.

---
+++ src/FilamentPagesServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MadeForYou\FilamentPages;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
+
+class FilamentPagesServiceProvider extends PackageServiceProvider
+{
+	public static string $name = 'made-filament-pages';
+
+	#[\Override] public function configurePackage(
+		Package $package
+	): void {
+		$package->name(self::$name)
+            ->hasConfigFile()
+            ->hasInstallCommand(function (InstallCommand $installCommand) {
+                $installCommand->startWith(
+                    fn (InstallCommand $command) => $command->info(
+                        'Let\'s install the Made for you filament pages package.'
+                    )
+                )
+                    ->publishConfigFile();
+            });
+	}
+}
